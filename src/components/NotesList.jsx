4432aeafede9d7f5673e95ac06a951f924aac651
Gianluca Procopio <gianplay024@gmail.com>
2026-03-27 00:49:51 -0300

    I created a Note Component to be added to the Note List

--- src/components/NotesList.jsx
+++ src/components/NotesList.jsx
@@ -1,3 +1,5 @@
+import Note from "./Note";
+
 const NotesList = ({notes, deleteNote}) => {
     
 
@@ -9,24 +11,7 @@ const NotesList = ({notes, deleteNote}) => {
     return (  
         <div className="space-y-4">
             {notes.map((note) =>(
-                <div 
-                key={note.id} 
-                className={'p-4 bg-white rounded-lg shadow-md border-l-4'}
-                style={{
-                    borderLeftColor: note.priority == 'High'? 'red': note.priority == 'Medium' ? 'orange': 'green'
-                }}
-                >
-                    <h3 className="text-lg font-bold">{note.title}</h3>
-                    <p className="text-sm text-gray-600">
-                        <strong>Category: {note.category}</strong>
-                    </p>
-                    <p className="text-sm text-gray-600">
-                        <strong>Priority: {note.priority}</strong>
-                    </p>
-                    <p className="mt-2">{note.description}</p>
-
-                    <button onClick={() => deleteNote(note.id)} className="mt-3 p-2 cursor-pointer bg-red-700 text-white hover:bg-red-800 transition-all rounded-lg relative left-90">Delete</button>
-                </div>
+                <Note key={note.id} note={note} deleteNote={deleteNote} />
             ))}
 
         </div> 
